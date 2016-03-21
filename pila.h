1f00e4b03e0551ff0ead984a40b9f5ac53d8270b
EliothMonroy <eliothmonroy@yahoo.com.mx>
2016-03-20 21:52:41 -0600

    Estructura de la pila
    Y funciones pop, y push en pila.h

--- pila.h
+++ pila.h
@@ -1,5 +1,15 @@
 #ifndef PILA__H__
 #define PILA__H__
 
+struct pilas
+{
+	int elementos_pila[100];
+	int ultimo_agregado;
+}
+
+typedef struct pilas Pila;
+
+int pop(Pila *, int);//Devuelve si funciono o no.
+int push(Pila *);//Devuelve el último elemento que tenga la pila.
 
 #endif
