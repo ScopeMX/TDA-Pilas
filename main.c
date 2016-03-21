ff512deb59c0cb925c702d25e2c8d50aed80076d
EliothMonroy <eliothmonroy@yahoo.com.mx>
2016-03-20 21:44:19 -0600

    Esqueletos de los arvchivos

---
+++ main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "pila.h"
+
+int main(int argc, char const *argv[])
+{
+	
+	return 0;
+}
---
+++ pila.h
@@ -0,0 +1,5 @@
+#ifndef PILA__H__
+#define PILA__H__
+
+
+#endif
