eccc8b854eb000e2cbc3ef21f8dd6a71f8fedb2d
Ian Mendoza Jaimes <ianmj2013@gmail.com>
2016-03-23 15:43:45 -0600

    inciamos

--- main.c
+++ main.c
@@ -4,6 +4,24 @@
 
 int main(int argc, char const *argv[])
 {
-	
+	Pila validar;
+	FILE *archivo = NULL;
+	char caracter = ' ';
+
+	validar.ultimo_agregado=0;
+
+	archivo = fopen(argv[1], "r");
+	if(archivo == NULL){
+		printf("%s\n", "Error al abrir el archivo");
+		exit(0);
+	}
+
+	while(feof(archivo)==0){
+		caracter = fgetc(archivo);
+		printf("%c", caracter);
+	}
+
+	fclose(archivo);
+
 	return 0;
 }
--- pila.h
+++ pila.h
@@ -5,12 +5,12 @@ struct pilas
 {
 	int elementos_pila[200];
 	int ultimo_agregado;
-}
+};
 
 typedef struct pilas Pila;
 //Función que saca el último elemento de la pila
 int pop(Pila *, int);//Devuelve el último elemento que tenga la pila.
 //Función que mete un elemento a la pila
-int push(Pila *);//Devuelve si funciono o no.
+int push(Pila *);//Devuelve 1 si funciono 0 si no
 
 #endif
