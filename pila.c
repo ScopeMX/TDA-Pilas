143520bd18a6aa134c22a81bf97a8b8e338c4082
EliothMonroy <eliothmonroy@yahoo.com.mx>
2016-03-24 00:06:47 -0600

    Se me fue un if

--- pila.c
+++ pila.c
@@ -3,8 +3,15 @@
 
 //Función que mete elementos a la pila
 int push(Pila * A, int elemento){
-	A->ultimo_agregado+=1;
-	A->elementos_pila[A->ultimo_agregado]=elemento;
+	//Si es la primera vez que se le va a agregar deberia de llegar como NULL la variable
+	if(A->ultimo_agregado==NULL){
+		A->ultimo_agregado=0;
+		A->elementos_pila[A->ultimo_agregado]=elemento;
+	}else{
+		A->ultimo_agregado+=1;
+		A->elementos_pila[A->ultimo_agregado]=elemento;
+	}
+	return 1;
 }
 
 int pop(Pila * A){
