cb578f7107fb301c42e890d564ecb398197e9b5c
EliothMonroy <eliothmonroy@yahoo.com.mx>
2016-03-23 23:54:09 -0600

    Según yo las funciones push y pop

--- pila.c
+++ pila.c
@@ -1,2 +1,12 @@
 #include <stdio.h>
 #include "pila.h"
+
+//Función que mete elementos a la pila
+int push(Pila * A, int elemento){
+	A->ultimo_agregado+=1;
+	A->elementos_pila[A->ultimo_agregado]=elemento;
+}
+
+int pop(Pila * A){
+	return A->elementos_pila[A->ultimo_agregado];
+}
